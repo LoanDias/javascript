8a3e0801babb00cc4f43622f53d036c402571c7c
LoanDias <loandias00@gmail.com>
2022-05-17 21:36:38 -0300

    att 0005

---
+++ exercicios/ex008/javi.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 80) { //condição simples
+    console.log(`Você ultrapassou a velocidade permitida da via. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
---
+++ exercicios/ex009/javi.js
@@ -0,0 +1,8 @@
+var país = 'França'
+console.log(`Nasceu em ${país}`)
+if (país == 'Brasil') {
+    console.log('Voce é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
+
