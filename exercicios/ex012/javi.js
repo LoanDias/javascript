fa9ccc93be554e1ea580a9c238679fbd03cd317a
LoanDias <loandias00@gmail.com>
2022-05-18 16:06:59 -0300

    att0006

---
+++ exercicios/ex012/javi.js
@@ -0,0 +1,10 @@
+var idade = 16
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log(' Você não vota.')
+}else if (idade < 18 | idade > 65) {
+   console.log('O seu voto é opcional.')
+}else {
+    console.log('O seu voto é obrigatório.')
+}
+//programa para dizer se vc vota ou não :)
