da7ec813a6fc3f3ad91a0796ced0d11915118067
LoanDias <loandias00@gmail.com>
2022-05-19 15:17:31 -0300

    att0007

--- testes/ambi.js
+++ testes/ambi.js
@@ -3,3 +3,20 @@ while (b <= 10) {
     console.log('Olá')
     b++   
 }
+
+
+
+//os dois fazem a mesma coisa mas o de cima é mais prático
+
+/*
+console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+*/
---
+++ testes/ambi2.js
@@ -0,0 +1,5 @@
+var l1 = 1
+do {
+    console.log(`passo ${l1}`)
+    l1++
+} while (l1 <= 10)
