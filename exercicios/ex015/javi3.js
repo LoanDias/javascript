89a045a2e27e87785509c1f6ff75ac5f196d2b3a
LoanDias <loandias00@gmail.com>
2022-05-23 13:48:04 -0300

    att 0008

--- exercicios/ex015/javi3.js
+++ exercicios/ex015/javi3.js
@@ -1,4 +1,5 @@
 let num = [5, 6, 7, 9, 45, 98, 4, 55, 62, 42]
 num.sort((function(a, b){return a-b}))
+console.log(`${num}`)
 console.log(num.indexOf(42))
-console.log(num.indexOf(32))
+console.log(num.indexOf(32))  
---
+++ testes/ambi5.js
@@ -0,0 +1,4 @@
+let num = [5, 3, 4 ,9 ,8, 7]
+num.sort((function (a, b){return a-b}))
+n1 = num[0] + num[3]
+console.log(`${n1}`)
