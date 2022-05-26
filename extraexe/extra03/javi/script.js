f6120198133e107a840f3353a5dd12edff14c00d
LoanDias <loandias00@gmail.com>
2022-05-26 15:04:14 -0300

    Update script.js

--- extraexe/extra03/javi/script.js
+++ extraexe/extra03/javi/script.js
@@ -1,13 +1,15 @@
 function contar() {
     var ini = document.getElementById('inicio')
     var fim = document.getElementById('final')
-    var pas = document.getElementById('passo')
+    //var pas = document.getElementById('passo')
+    var pas = 0
     var res = document.getElementById('resultado')
-    if (ini == 0 || fim == 0) {
+    if (ini.value.lenght == 0 || Number(fim.value) == 0) {
         res.innerHTML = 'Impossível contar'
         alert('Por favor, verique se os campos foram preenchidos corretamente')
-    } else if (pas == 0){
-        alert('Passo inválido, considerando Passo: 1')
-        for(n1 = ini;)
+    } else if (Number(pas.value) == 0){
+        alert('Passo inválido! Considerando Passo: 1')
+        for(var n1=Number(ini.value);n1<=Number(fim.value);n1++)
+        res.innerHTML = `Passo ${n1}`
     }
 }
