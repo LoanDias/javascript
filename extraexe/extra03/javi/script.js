8125d7d7874c50ac073d9d6e221315846eeb54ae
LoanDias <loandias00@gmail.com>
2022-05-26 14:08:12 -0300

    att 0014

---
+++ extraexe/extra03/javi/script.js
@@ -0,0 +1,13 @@
+function contar() {
+    var ini = document.getElementById('inicio')
+    var fim = document.getElementById('final')
+    var pas = document.getElementById('passo')
+    var res = document.getElementById('resultado')
+    if (ini == 0 || fim == 0) {
+        res.innerHTML = 'Impossível contar'
+        alert('Por favor, verique se os campos foram preenchidos corretamente')
+    } else if (pas == 0){
+        alert('Passo inválido, considerando Passo: 1')
+        for(n1 = ini;)
+    }
+}
